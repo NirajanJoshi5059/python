be06dbe89c8bf7bde7b78f00838620c0bfe3feb6
NirajanJoc <Nirajanjoc19@gmail.com>
2022-03-31 15:03:06 +0545

    area_conversion

---
+++ area_conversion.py
@@ -0,0 +1,68 @@
+#Area conversion
+#millimeter into centimeter.
+def mm_to_cm(mm):
+    cm=mm*0.1
+    return cm
+#milimeter into inch.
+def mm_to_inch(mm):
+    inch=mm*0.03937008
+    return inch
+#millimeter into feet.
+def mm_to_ft(mm):
+    feet=mm*0.00328084
+    return feet
+#millimeter into meter.
+def mm_to_m(mm):
+    meter=mm*0.001
+    return meter
+
+#Centimeter into millimeter.
+def cm_to_mm(cm):
+    mm=cm*10
+    return mm
+#Centimeter into inch.
+def cm_to_inch(cm):
+    inch=cm*.3937008
+    return inch
+#centimeter into feet.
+def cm_to_ft(cm):
+    feet=cm*.0328084
+    return feet
+#centimeter into meter.
+def cm_to_m(cm):
+    meter=cm*0.01
+    return meter
+
+#feet into millimeter.
+def ft_to_mm(ft):
+    mm=ft*304.8
+    return mm
+#feet into centimeter.
+def ft_to_cm(ft):
+    cm=ft*30.48
+    return cm
+#feet into inch.
+def ft_to_inch(ft):
+    inch=ft*12
+    return inch
+#feet into meter.
+def ft_to_m(ft):
+    mm=ft*0.3048
+    return mm
+
+#meter into millimeter.
+def m_to_mm(m):
+    mm=m*1000
+    return mm
+#meter into centimeter.
+def m_to_cm(m):
+    cm=m*100
+    return cm
+#meter into inch.
+def m_to_inch(m):
+    inch=m*39.37008
+    return inch
+#meter into feet.
+def m_to_ft(m):
+    feet=m*3.28084
+    return feet
